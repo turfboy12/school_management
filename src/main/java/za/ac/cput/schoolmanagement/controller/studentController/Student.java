d8e7b6656d0f2715389ba675c59e84fa54572371
HonestMpungu <Mpunguhonest@gmail.com>
2022-06-17 08:23:08 +0200

    test

---
+++ src/main/java/za/ac/cput/schoolmanagement/controller/studentController/Student.java
@@ -0,0 +1,13 @@
+package za.ac.cput.schoolmanagement.controller.studentController;
+
+/*
+ * Entity for the Genre
+ * Project Term One
+ * Author: Honest Mpungu
+ * Student number 215072081
+ * Date: 10 April 2022
+ */
+
+
+public class Student {
+}
